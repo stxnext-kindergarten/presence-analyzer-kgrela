3ca79869d8614be7207ac413d4a8bcd6a408ef47
kamil grela <kamilgrela97@gmail.com>
2014-04-08 17:00:17 +0200

    [1720] Correcting import syntax

--- src/run.py
+++ src/run.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Presence analyzer web app.
-"""
+"""Presence analyzer web app."""
 import os.path
 import logging.config
 
 from presence_analyzer.main import app
-import presence_analyzer.views
+from presence_analyzer import views
 
 
 if __name__ == "__main__":
